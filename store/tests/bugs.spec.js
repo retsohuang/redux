de3f4ab419ba5b34733dcbc2120734e3b3def841
Retso <retsohuang@gmail.com>
2022-04-09 10:20:26 +0800

    test(bugs): add addBug success and fail tests

--- store/tests/bugs.spec.js
+++ store/tests/bugs.spec.js
@@ -2,19 +2,31 @@ import { addBug } from '@/store/bugs'
 import createStore from '@/store/createStore'
 
 describe('bugsSlice', () => {
+  let store
+
+  const bugsSlice = () => store.getState().entities.bugs
+
   beforeEach(() => {
+    store = createStore()
     fetch.resetMocks()
   })
 
-  it('should handle the addBug action', async () => {
-    const store = createStore()
+  it(`should add the bug to the store if it's saved to the server`, async () => {
     const bug = { description: 'a' }
     const savedBug = { ...bug, id: 1 }
-
     fetchMock.mockResponseOnce(JSON.stringify(savedBug))
 
     await store.dispatch(addBug(bug))
 
-    expect(store.getState().entities.bugs.list).toContainEqual(savedBug)
+    expect(bugsSlice().list).toContainEqual(savedBug)
+  })
+
+  it(`should not add the bug to the store if it's not saved to the server`, async () => {
+    const bug = { description: 'a' }
+    fetchMock.mockResponseOnce(JSON.stringify({}), { status: 500 })
+
+    await store.dispatch(addBug(bug))
+
+    expect(bugsSlice().list).toHaveLength(0)
   })
 })
